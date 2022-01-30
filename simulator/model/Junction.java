b90e95201f591c23167f272f15f65adb588e4e1c
Jaime Jiménez <jaimejimenez99@hotmail.com>
2022-01-30 19:45:11 +0100

    Added Junction class

---
+++ simulator/model/Junction.java
@@ -0,0 +1,24 @@
+package simulator.model;
+
+import org.json.JSONObject;
+
+public class Junction extends SimulatedObject {
+
+	Junction(String id) {
+		super(id);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	void advance(int time) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public JSONObject report() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
