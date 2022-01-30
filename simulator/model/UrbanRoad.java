76b1ff41722d663f6c8032701a2c1a080a6f95e6
Jaime Jiménez <jaimejimenez99@hotmail.com>
2022-01-30 20:37:26 +0100

    Added Extended classes of Road and LightingSwitchingStrategy interface2

---
+++ simulator/model/UrbanRoad.java
@@ -0,0 +1,29 @@
+package simulator.model;
+
+public class UrbanRoad extends Road {
+
+	UrbanRoad(String id, Junction srcJunc, Junction destJunc, int maxSpeed, int contLimit, int length,
+			Weather weather) {
+		super(id, srcJunc, destJunc, maxSpeed, contLimit, length, weather);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	void reduceTotalContamination() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void updateSpeedLimit() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	int calculateVehicleSpeed(Vehicle v) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
