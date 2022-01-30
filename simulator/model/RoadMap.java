b27bfecfdf3ebd836c59c09e1749b9596e02edec
Jaime Jiménez <jaimejimenez99@hotmail.com>
2022-01-30 20:30:54 +0100

    Added RoadMap class

---
+++ simulator/model/RoadMap.java
@@ -0,0 +1,27 @@
+package simulator.model;
+
+public class RoadMap extends Road {
+
+	RoadMap(String id, Junction srcJunc, Junction destJunc, int maxSpeed, int contLimit, int length, Weather weather) {
+		super(id, srcJunc, destJunc, maxSpeed, contLimit, length, weather);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	void reduceTotalContamination() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void updateSpeedLimit() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	int calculateVehicleSpeed(Vehicle v) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
