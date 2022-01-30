3752d7420584a4bff837e639c74174679ff16216
Jaime Jiménez <jaimejimenez99@hotmail.com>
2022-01-30 20:26:03 +0100

    Added classes that implements DequeingStrategy class

---
+++ simulator/model/MoveFirstStrategy.java
@@ -0,0 +1,13 @@
+package simulator.model;
+
+import java.util.List;
+
+public class MoveFirstStrategy implements DequeingStrategy {
+
+    @Override
+    public List<Vehicle> dequeue(List<Vehicle> q) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
