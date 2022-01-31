78e47bd5f904ca1c671ea58139d7bc1f660173c2
Jaime Jiménez <jaimejimenez99@hotmail.com>
2022-02-01 00:04:58 +0100

    Added coded RoadMap class

--- simulator/model/RoadMap.java
+++ simulator/model/RoadMap.java
@@ -1,27 +1,73 @@
 package simulator.model;
 
-public class RoadMap extends Road {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-	RoadMap(String id, Junction srcJunc, Junction destJunc, int maxSpeed, int contLimit, int length, Weather weather) {
-		super(id, srcJunc, destJunc, maxSpeed, contLimit, length, weather);
-		// TODO Auto-generated constructor stub
+import org.json.JSONObject;
+
+public class RoadMap {
+
+	private List<Junction> junctions;
+	private List<Road> roads;
+	private List<Vehicle> vehicles;
+	private Map<String, Junction> junctionMap;
+	private Map<String, Vehicle> vehiclesMap;
+
+
+	RoadMap() {
+		reset();
 	}
 
-	@Override
-	void reduceTotalContamination() {
-		// TODO Auto-generated method stub
-		
+	void reset() {
+		junctions = new ArrayList<>();
+		roads = new ArrayList<>();
+		vehicles = new ArrayList<>();
+		junctionMap = new HashMap();
+		vehiclesMap = new HashMap();
 	}
 
-	@Override
-	void updateSpeedLimit() {
-		// TODO Auto-generated method stub
-		
+	void addJunction(Junction junction) {
+		if (!junctionMap.containsKey(junction.getId())) {
+			junctionMap.put(junction.getId(), junction);
+		}
 	}
 
-	@Override
-	int calculateVehicleSpeed(Vehicle v) {
-		// TODO Auto-generated method stub
-		return 0;
+	void addRoad(Road road) {
+
+	}
+
+	void addVehicle(Vehicle v) {
+
+	}
+
+	public Junction getJunction(String id) { return junctionMap.get(id); }
+
+	public Road getRoad(String id) {
+		for (Road r : roads) 
+			if (r.getId() == id)
+				return r;
+		return null;
+	}
+
+	public Vehicle getVehicle(String id) { return vehiclesMap.get(id); }
+
+	public List<Vehicle> getVehicles() { return Collections.unmodifiableList(new ArrayList<>(vehicles)); }
+
+	public List<Road> getRoads() { return Collections.unmodifiableList(new ArrayList<>(roads)); }
+
+	public List<Junction> getJunction() { return Collections.unmodifiableList(new ArrayList<>(junctions)); }
+	
+
+	public JSONObject report() {
+		JSONObject data = new JSONObject();
+
+		data.put("junctions", junctions.toString());
+		data.put("road", roads.toString());
+		data.put("vehicles", vehicles.toString());
+
+		return data;
 	}
 }
