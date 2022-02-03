402de9d15b1bd661fcf2101ef011a1a8b1aa0d6e
Jaime Jiménez <jaimejimenez99@hotmail.com>
2022-02-03 14:50:26 +0100

    Implemented SetWeatherEvent class

--- simulator/model/SetWeatherEvent.java
+++ simulator/model/SetWeatherEvent.java
@@ -1,16 +1,27 @@
 package simulator.model;
 
+import java.util.List;
+
+import simulator.misc.Pair;
+
 public class SetWeatherEvent extends Event {
 
-    SetWeatherEvent(int time) {
-        super(time);
-        //TODO Auto-generated constructor stub
-    }
+    private List<Pair<String, Weather>> ws;
+	
+	SetWeatherEvent(int time, List<Pair<String, Weather>> ws) {
+		super(time);
+		if (ws.isEmpty())
+			throw new IllegalArgumentException("List empty");
+		this.ws = ws;
+	}
 
-    @Override
-    void execute(RoadMap map) {
-        // TODO Auto-generated method stub
-        
-    }
+	@Override
+	void execute(RoadMap map) {
+		for (Pair<String, Weather> w : ws) {
+			if (map.getRoad(w.getFirst()) == null)
+				throw new IllegalArgumentException("Road doesnt exist");
+			map.getRoad(w.getFirst()).setWeather(w.getSecond());
+		}
+	}
     
 }
