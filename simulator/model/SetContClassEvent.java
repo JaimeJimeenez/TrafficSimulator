53d4add747bdbcec728d92962290d9f791d4c5ee
Jaime Jiménez <jaimejimenez99@hotmail.com>
2022-02-03 14:48:27 +0100

    Implemented SetContClassEvent class

--- simulator/model/SetContClassEvent.java
+++ simulator/model/SetContClassEvent.java
@@ -1,16 +1,27 @@
 package simulator.model;
 
+import java.util.List;
+
+import simulator.misc.Pair;
+
 public class SetContClassEvent extends Event {
 
-    SetContClassEvent(int time) {
-        super(time);
-        //TODO Auto-generated constructor stub
-    }
+    private List<Pair<String, Integer>> cs;
+	
+	SetContClassEvent(int time, List<Pair<String, Integer>> cs) {
+		super(time);
+		if (cs == null)
+			throw new IllegalArgumentException("List null");
+		this.cs = cs;
+	}
 
-    @Override
-    void execute(RoadMap map) {
-        // TODO Auto-generated method stub
-        
-    }
+	@Override
+	void execute(RoadMap map) {
+		for (Pair<String, Integer> c : cs) {
+			if (map.getVehicle(c.getFirst()) == null)
+				throw new IllegalArgumentException("Vehicle null");
+			map.getVehicle(c.getFirst()).setContaminationClass(c.getSecond());
+		}
+	}
     
 }
