0ebbca1c86463546b10335f1f09f91a59f48b88e
Jaime Jiménez <jaimejimenez99@hotmail.com>
2022-02-07 13:43:27 +0100

    Implemented Strategies of DequeuingStrategy

--- simulator/model/MostCrowdedStrategy.java
+++ simulator/model/MostCrowdedStrategy.java
@@ -4,19 +4,39 @@ import java.util.List;
 
 public class MostCrowdedStrategy implements LightSwitchingStrategy {
 
-    private int timeSlot;
-
-    public MostCrowdedStrategy(int timeSlot) {
-        this.timeSlot = timeSlot;
-    }
-
-    @Override
-    public int chooseNextGreen(List<Road> roads, List<List<Vehicle>> qs, int currGreen, int lastSwitchingTime, int currTime) {
-        
-        if (roads.isEmpty())
-            return -1;
-        //TODO
-        return 0;
-    }
+	private int timeSlot;
+	
+	public MostCrowdedStrategy(int timeSlot) {
+		this.timeSlot = timeSlot;
+	}
+	
+	@Override
+	public int chooseNextGreen(List<Road> roads, List<List<Vehicle>> qs, int currGreen, int lastSwitchingTime,
+			int currTime) {
+		if (roads.isEmpty())
+			return -1;
+		else if (currGreen == -1) {
+			int maxSize = -1;
+			for (int i = 0; i < qs.size(); i++) {
+				if (qs.get(i).size() > maxSize) 
+					maxSize = i;
+			}
+			return maxSize;
+		}
+		else if (currTime - lastSwitchingTime < timeSlot)
+			return currGreen;
+		else {
+			int next = -1;
+			for (int i = currGreen + 1; i < qs.size(); i++) {
+				if (qs.get(i).size() > next)
+					next = i;
+				if (i + 1 == qs.size())
+					i = 0;
+				if (i == currGreen)
+					break;
+			}
+			return next;
+		}
+	}
 
 }
--- simulator/model/MoveAllStrategy.java
+++ simulator/model/MoveAllStrategy.java
@@ -1,13 +1,13 @@
 package simulator.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MoveAllStrategy implements DequeuingStrategy {
 
     @Override
     public List<Vehicle> dequeue(List<Vehicle> q) {
-        // TODO Auto-generated method stub
-        return null;
+        return new ArrayList<Vehicle>(q);
     }
     
 }
