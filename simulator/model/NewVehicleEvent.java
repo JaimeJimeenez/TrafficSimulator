78dc5ee3b8c97f26c8436547307717d653f7f974
Jaime Jiménez <jaimejimenez99@hotmail.com>
2022-02-07 13:49:07 +0100

    Modified NewVehicleEvent class

--- simulator/model/NewVehicleEvent.java
+++ simulator/model/NewVehicleEvent.java
@@ -1,28 +1,30 @@
 package simulator.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class NewVehicleEvent extends Event {
 
-    private String id;
-    private List<Junction> itinerary;
-    private int maxSpeed, speed, location, contClass, totalCont, totalDistance;
-    private VehicleStatus status;
-    private Road road;
+    String id;
+	int maxSpeed, contClass;
+	List<String> itinerary;
+	
+	public NewVehicleEvent(int time, String id, int maxSpeed, int contClass, List<String> itinerary) {
+		super(time);
+		this.id = id;
+		this.maxSpeed = maxSpeed;
+		this.contClass = contClass;
+		this.itinerary = itinerary;
+	}
 
-    public NewVehicleEvent(int time, String id, int maxSpeed, int contClass, List<Junction> itinerary) {
-        super(time);
-        this.id = id;
-        this.maxSpeed = maxSpeed;
-        this.contClass = contClass;
-        this.itinerary = itinerary;
-    }
+	@Override
+	void execute(RoadMap map) {
+		List<Junction> junctions = new ArrayList<>();
+		for (String s : itinerary)
+			junctions.add(map.getJunction(s));
+		Vehicle newVehicle = new Vehicle(id, contClass, contClass, junctions);
+		map.addVehicle(newVehicle);
+		newVehicle.moveToNextRoad();
+	}
 
-    @Override
-    void execute(RoadMap map) {
-        Vehicle v = new Vehicle(id, maxSpeed, contClass, itinerary);
-        map.addVehicle(v);
-        v.moveToNextRoad();
-    }
-    
 }
