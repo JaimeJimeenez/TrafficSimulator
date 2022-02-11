798c9be88f8dc11a264e7c63221254c5cbdb9cf5
Jaime Jiménez <jaimejimenez99@hotmail.com>
2022-02-11 13:56:19 +0100

    Completed NewInterCityRoad class

--- simulator/model/NewInterCityRoadEvent.java
+++ simulator/model/NewInterCityRoadEvent.java
@@ -2,17 +2,13 @@ package simulator.model;
 
 public class NewInterCityRoadEvent extends NewRoadEvent {
 
-    String id, srcJunc, destJunc;
-	int length, co2Limit, maxSpeed;
-	Weather weather;
-	
 	public NewInterCityRoadEvent(int time, String id, String srcJunc, String destJunc, int length, int co2Limit, int maxSpeed, Weather weather) {
 		super(time, id, srcJunc, destJunc, length, co2Limit, maxSpeed, weather);
 	}
 
 	@Override
 	protected Road createRoadObject() {
-		return null;
+		return new InterCityRoad(id, src, dest, maxSpeed, co2Limit, length, weather);
 	}
 
 }
