7702b5e31dd747bc17198aaa3633397a34b08674
Jaime Jiménez <jaimejimenez99@hotmail.com>
2022-02-18 13:32:20 +0100

    Modified RoadMap class

--- simulator/model/RoadMap.java
+++ simulator/model/RoadMap.java
@@ -10,88 +10,100 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class RoadMap {
-
+	
 	private List<Junction> junctions;
 	private List<Road> roads;
 	private List<Vehicle> vehicles;
-	private Map<String, Junction> junctionMap;
+	private Map<String, Junction> junctionsMap;
+	private Map<String, Road> roadsMap;
 	private Map<String, Vehicle> vehiclesMap;
 
-	RoadMap() {
-		reset();
-	}
+	RoadMap() { reset(); }
 	
 	void reset() {
 		junctions = new ArrayList<>();
 		roads = new ArrayList<>();
 		vehicles = new ArrayList<>();
-		junctionMap = new HashMap<>();
+		junctionsMap = new HashMap<>();
+		roadsMap = new HashMap<>();
 		vehiclesMap = new HashMap<>();
 	}
-	
+
 	void addJunction(Junction junction) {
-		if (junctions.contains(junction))
-			throw new IllegalArgumentException("Junction already exists");
-		junctionMap.put(junction.getId(), junction);
+		if (junctionsMap.containsKey(junction.getId()))
+			throw new IllegalArgumentException("Error: Some junction already has the same id");
+		
 		junctions.add(junction);
+		junctionsMap.put(junction.getId(), junction);
 	}
+	
+	@SuppressWarnings("unlikely-arg-type")
+	private boolean checkJunctionsRoad(Road road) { return roadsMap.containsValue(road.getSrc()) && roadsMap.containsValue(road.getDest()); }
 	
 	void addRoad(Road road) {
-		if (roads.contains((road))) // TODO Complete the if clause
-			throw new IllegalArgumentException("Error: Cannot add this road");
+		if (roadsMap.containsKey(road.getId()) || !checkJunctionsRoad(road))
+			throw new IllegalArgumentException("Error: Junctions don't exist or some road already has the same id");
+		
 		roads.add(road);
+		roadsMap.put(road.getId(), road);
 	}
 	
-	void addVehicle(Vehicle v) {
-		if (vehicles.contains(v)) // TODO Complete the if clause
-			throw new IllegalArgumentException("Error: Cannot add this vahicle");
-		vehicles.add(v);
-		vehiclesMap.put(v.getId(), v);
+	private boolean checkItinerary(List<Junction> itinerary) { //??
+		
+		for (int i = 0; i < itinerary.size(); i++) 
+			if (itinerary.get(i).roadTo(itinerary.get(i + 1)) == null)
+				return true;
+		return false;
 	}
 	
-	public Junction getJunction(String id) { return junctionMap.get(id); }
-	
-	public Road getRoad(String id) { 
-		for (Road r : roads) 
-			if (r.getId() == id)
-				return r;
-		return null;
+	void addVehicle(Vehicle vehicle) {
+		if (vehiclesMap.containsKey(vehicle.getId()) || checkItinerary(vehicle.getItinerary())) 
+			throw new IllegalArgumentException("Error: Itinerary not valid or some vehicle already has the same id");
+		
+		vehicles.add(vehicle);
+		vehiclesMap.put(vehicle.getId(), vehicle);
+		vehicle.moveToNextRoad(); //?
 	}
+	
+	public Junction getJunction(String id) { return junctionsMap.get(id); }
+	
+	public Road getRoad(String id) { return roadsMap.get(id); }
 	
 	public Vehicle getVehicle(String id) { return vehiclesMap.get(id); }
 	
-	public List<Vehicle> getVehicles() { return Collections.unmodifiableList(new ArrayList<>(vehicles)); }
+	public List<Junction> getJunctions() { return Collections.unmodifiableList(junctions); }
 	
-	public List<Road> getRoads() { return Collections.unmodifiableList(new ArrayList<>(roads)); }
+	public List<Road> getRoads() { return Collections.unmodifiableList(roads); }
 	
-	public List<Junction> getJunctions() { return Collections.unmodifiableList(new ArrayList<>(junctions)); }
+	public List<Vehicle> getVehicles() { return Collections.unmodifiableList(vehicles); }
 	
 	private JSONArray getDataJunctions() {
 		JSONArray data = new JSONArray();
-
-		for (Junction j : junctions)
+		
+		for (Junction j : junctions) 
 			data.put(j.report());
-
+		
 		return data;
 	}
-
+	
 	private JSONArray getDataRoads() {
 		JSONArray data = new JSONArray();
-
+		
 		for (Road r : roads)
 			data.put(r.report());
-
+		
 		return data;
 	}
-
+	
 	private JSONArray getDataVehicles() {
 		JSONArray data = new JSONArray();
-
+		
 		for (Vehicle v : vehicles)
 			data.put(v.report());
-
+		
 		return data;
 	}
+	
 	public JSONObject report() {
 		JSONObject data = new JSONObject();
 		
@@ -101,4 +113,5 @@ public class RoadMap {
 		
 		return data;
 	}
+	
 }
