de1255c57e866f203f7b3dbe4695beca54f8adf6
Jaime Jiménez <jaimejimenez99@hotmail.com>
2022-02-19 12:59:40 +0100

    Modified Road class

--- simulator/model/Road.java
+++ simulator/model/Road.java
@@ -9,35 +9,36 @@ import org.json.JSONObject;
 import simulator.model.Vehicle.CompareLocation;
 
 abstract public class Road extends SimulatedObject {
-
+	
 	int length, contLimit, maxSpeed, totalCO2, speedLimit;
 	Junction dest, src;
 	Weather weather;
 	List<Vehicle> vehicles;
 	
-	Road(String id, Junction srcJunc, Junction destJunc, int maxSpeed, int contLimit, int length, Weather weather) {
+	Road(String id, Junction dest, Junction src, int length, int contLimit, int maxSpeed, Weather weather) {
 		super(id);
-		if (maxSpeed < 0 || contLimit < 0 || length < 0 || srcJunc != null || destJunc != null || weather != null)
-			throw new IllegalArgumentException("Arguments not valid");
+		if (maxSpeed < 0 || contLimit < 0 || length < 0 || src == null || dest == null || weather == null)
+			throw new IllegalArgumentException("Argument(s) not valid");
 		
-		this.src = srcJunc;
-		this.dest = destJunc;
+		this.dest = dest;
+		this.src = src;
 		this.length = length;
-		this.maxSpeed = maxSpeed;
-		this.speedLimit = maxSpeed;
 		this.contLimit = contLimit;
+		this.maxSpeed = maxSpeed;
 		this.totalCO2 = 0;
+		this.speedLimit = maxSpeed;
 		this.weather = weather;
-		this.vehicles = new ArrayList<>();
+		this.vehicles = new ArrayList<Vehicle>();
+		
+		src.addOutGoingRoad(this);
 		dest.addIncomingRoad(this);
-		src.addOutgoingRoad(this);
 	}
 	
 	public int getLength() { return length; }
 	
-	public Junction getDest() { return dest; }
-	
 	public Junction getSrc() { return src; }
+	
+	public Junction getDest() { return dest; }
 	
 	Weather getWeather() { return weather; }
 	
@@ -49,40 +50,42 @@ abstract public class Road extends SimulatedObject {
 	
 	int getSpeedLimit() { return speedLimit; }
 	
-	public List<Vehicle> getVehicles() { return Collections.unmodifiableList(new ArrayList<>(vehicles)); }
+	public List<Vehicle> getVehicles() { return Collections.unmodifiableList(vehicles); }
 	
-	void setWeather(Weather w) {
-		if (w != null)
-			throw new IllegalArgumentException("Weather is null");
-		this.weather = w;
+	void setWeather(Weather weather) {
+		if (weather == null)
+			throw new IllegalArgumentException("Weather object not valid");
+		this.weather = weather;
 	}
-
-	void addContamination(int c) {
-		if (c < 0)
+	
+	void addContamination(int cont) {
+		if (cont < 0)
 			throw new IllegalArgumentException("Contamination negative");
-		totalCO2 += c;
+		totalCO2 += cont;
 	}
-
+	
 	void enter(Vehicle v) {
 		if (v.getLocation() != 0 || v.getSpeed() != 0)
-			throw new IllegalArgumentException("Vehicle's speed and/or locaction is not 0");
+			throw new IllegalArgumentException("Location or speed not 0");
 		vehicles.add(v);
 	}
 	
-	void exit(Vehicle v) {	
-		if (vehicles.isEmpty())
-			throw new IllegalArgumentException("There are no vehicles on the road");
-		vehicles.remove(v); 
+	void exit(Vehicle v) {
+		if (!vehicles.contains(v))
+			throw new IllegalArgumentException("Vehicle doesnt exist");
+		vehicles.remove(v);
 	}
 	
 	public void advance(int time) {
-		reduceTotalContamination();
+		if (totalCO2 != 0)
+			reduceTotalContamination();
 		updateSpeedLimit();
-
+		
 		for (Vehicle v : vehicles) {
 			v.setSpeed(calculateVehicleSpeed(v));
-			updateSpeedLimit();
+			v.advance(time);
 		}
+		
 		Collections.sort(vehicles, new CompareLocation());
 	}
 	
@@ -91,15 +94,18 @@ abstract public class Road extends SimulatedObject {
 		
 		data.put("id", getId());
 		data.put("speedlimit", speedLimit);
+		data.put("weather", weather);
 		data.put("co2", totalCO2);
-		data.put("vehicles", vehicles.toString());
+		data.put("vehicles", vehicles.toString()); //?
 		
 		return data;
 	}
-
+	
 	abstract void reduceTotalContamination();
 	
 	abstract void updateSpeedLimit();
 	
 	abstract int calculateVehicleSpeed(Vehicle v);
+	
+	
 }
