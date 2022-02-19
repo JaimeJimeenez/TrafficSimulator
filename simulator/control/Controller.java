0fdda2265f099ec303ce7b7b2ce4ece2d5eac470
Jaime Jiménez <jaimejimenez99@hotmail.com>
2022-02-19 12:53:34 +0100

    Modified Controller class

--- simulator/control/Controller.java
+++ simulator/control/Controller.java
@@ -1,6 +1,5 @@
 package simulator.control;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
@@ -9,52 +8,47 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
-import simulator.factories.Factory;
 import simulator.model.Event;
+import simulator.factories.Factory;
 import simulator.model.TrafficSimulator;
 
 public class Controller {
-    TrafficSimulator simulator;
-    Factory<Event> eventsFactory;
-
-    public Controller(TrafficSimulator sim, Factory<Event> eventsFactory) {
-        this.simulator = sim;
-        this.eventsFactory = eventsFactory;
-    }
-
-    public void reset() { simulator.reset(); }
-
-    public void loadEvents(InputStream in) {
-        JSONObject jsonInput = new JSONObject(new JSONTokener(in));
-        JSONArray events = jsonInput.getJSONArray("events");
-
-        for (int i = 0; i < events.length(); i++) 
-            simulator.addEvent(eventsFactory.createInstance(events.getJSONObject(i)));
-    }
-
-    public void run(int steps, OutputStream out) {
-        JSONObject expOutJO = null;
-
-        if (out == null) {
-            out = new OutputStream() {
-                @Override
-                public void write(int b) throws IOException { }
-            };
-        }
-
-        PrintStream p = new PrintStream(out);
-        p.println("{");
-        p.println("\"states\": [");
-
-        JSONObject currState = null;
-        JSONObject expState = null;
-
-        currState = simulator.report();
-        p.print(currState);
-        if (expOutJO != null)
-            expState = expOutJO.getJSONArray("states").getJSONObject(0);
-        
-        for (int i = 0; i < expOutJO.length(); i++)
-            expState = expOutJO.getJSONArray("states").getJSONObject(i);
-    }
+	
+	TrafficSimulator simulator;
+	Factory<Event> eventsFactory;
+	
+	public Controller(TrafficSimulator sim, Factory<Event> eventsFactory) {
+		this.simulator = sim;
+		this.eventsFactory = eventsFactory;
+	}
+	
+	public void reset() { simulator.reset(); }
+	
+	public void loadEvents(InputStream in) {
+		JSONObject jsonInput = new JSONObject(new JSONTokener(in));
+		JSONArray events = jsonInput.getJSONArray("events");
+		
+		for (int i = 0; i < events.length(); i++) 
+			simulator.addEvent(eventsFactory.createInstance(events.getJSONObject(i)));
+		
+	}
+	
+	public void run(int steps, OutputStream out) {
+		
+		PrintStream p = new PrintStream(out);
+		p.println("{");
+		p.println("\t\"states\": [");
+		
+		for (int i = 0; i < steps - 1; i++) {
+			simulator.advance();
+			p.println(simulator.report().toString() + ",");
+		}
+		
+		simulator.advance();
+		p.println(simulator.report().toString());
+		p.print("]");
+		p.println("}");
+		
+	}
+	
 }
