6e5d3d33f5032effb0eb4e707536354a912cbae2
Jaime Jiménez <jaimejimenez99@hotmail.com>
2022-02-28 13:48:23 +0100

    Fixed error in MostCrowdedStrategy

--- simulator/model/MostCrowdedStrategy.java
+++ simulator/model/MostCrowdedStrategy.java
@@ -9,34 +9,30 @@ public class MostCrowdedStrategy implements LightSwitchingStrategy {
 	public MostCrowdedStrategy(int timeSlot) {
 		this.timeSlot = timeSlot;
 	}
+
+	private int getNextGreen(List<List<Vehicle>> vehicles, int position) {
+		int maxSize = -1;
+		int nextGreen = 0;
+
+		for (int i = position; i < vehicles.size(); i++) 
+			if (vehicles.get(i).size() > maxSize) {
+				nextGreen = i;
+				maxSize = vehicles.get(i).size();
+			}
+		
+		return nextGreen;
+	}
 	
 	@Override
 	public int chooseNextGreen(List<Road> roads, List<List<Vehicle>> qs, int currGreen, int lastSwitchingTime,
 			int currTime) {
 		if (roads.isEmpty())
 			return -1;
-		else if (currGreen == -1) {
-			int maxSize = -1;
-			for (int i = 0; i < qs.size(); i++) {
-				if (qs.get(i).size() > maxSize) 
-					maxSize = i;
-			}
-			return maxSize;
-		}
+		else if (currGreen == -1) 
+			return getNextGreen(qs, 0);
 		else if (currTime - lastSwitchingTime < timeSlot)
 			return currGreen;
-		else {
-			int next = -1;
-			for (int i = currGreen + 1; i < qs.size(); i++) {
-				if (qs.get(i).size() > next)
-					next = i;
-				if (i + 1 == qs.size())
-					i = 0;
-				if (i == currGreen)
-					break;
-			}
-			return next;
-		}
+		return getNextGreen(qs, (currGreen + 1) % roads.size());
 	}
 
 }
