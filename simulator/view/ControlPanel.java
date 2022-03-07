8fc02d6eca522a7c87fe2a9da17d7ee56ab8f4c2
Jaime Jiménez <jaimejimenez99@hotmail.com>
2022-03-07 14:28:29 +0100

    Added view folder

---
+++ simulator/view/ControlPanel.java
@@ -0,0 +1,19 @@
+package simulator.view;
+
+import javax.swing.JPanel;
+
+import simulator.control.Controller;
+
+public class ControlPanel extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+	
+	ControlPanel(Controller ctrl) {
+		initGUI(ctrl);
+	}
+
+	private void initGUI(Controller ctrl) {
+		
+	}
+	
+}
