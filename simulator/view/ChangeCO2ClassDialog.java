0f4a76c4c21fa05ec13ae2d239209a6583073aaa
Jaime Jiménez <jaimejimenez99@hotmail.com>
2022-03-14 15:16:29 +0100

    Advanced ChangeCO2ClassDialog class

--- simulator/view/ChangeCO2ClassDialog.java
+++ simulator/view/ChangeCO2ClassDialog.java
@@ -1,77 +1,138 @@
 package simulator.view;
 
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.FlowLayout;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
 
+import simulator.misc.Pair;
+import simulator.model.Vehicle;
+
+
 public class ChangeCO2ClassDialog extends JDialog {
-    
-    private static final long serialVersionUID = 1L;
 
-    private JSpinner ticksSpinner;
+	private static final long serialVersionUID = 1L;
 
-    private JButton cancelButton;
-    private JButton okButton;
+	private int status;
+	
+	private JComboBox<String> boxVehicles;
+	private JComboBox<Integer> boxCO2;
+	private JSpinner ticksSpinner;
+	private JButton cancelButton;
+	private JButton okButton;
 
-    ChangeCO2ClassDialog() {
-        initGui();
-    }
+	ChangeCO2ClassDialog() {
+		this.status = 0;
+		initGui();
+	}
+	
+	private void initGui()  {
+		JPanel mainPanel = new JPanel();
+		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+		add(mainPanel);
+		
+		//Description;
+		JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 2, 3));
+		mainPanel.add(topPanel);
+		
+		JLabel descLabel = new JLabel("Schedule an event to change the CO2 class of a vehicle after a given number o simulation ticks from now.");
+		topPanel.add(descLabel);
+		
+		// Vehicles
+		JPanel centerPanel = new JPanel();
+		mainPanel.add(centerPanel);
+		
+		JLabel vehiclesLabel = new JLabel("Vehicle:");
+		centerPanel.add(vehiclesLabel);
+		
+		boxVehicles = new JComboBox<String>();
+		boxVehicles.setPreferredSize(new Dimension(50, 25));
+		centerPanel.add(boxVehicles);
+		
+		// CO2 
+		JLabel co2Label = new JLabel("CO2 Class:");
+		centerPanel.add(co2Label);
+		
+		boxCO2 = new JComboBox<Integer>();
+		boxCO2.setPreferredSize(new Dimension(50, 25));
+		centerPanel.add(boxCO2);
+		
+		// Ticks
+		JLabel ticksLabel = new JLabel("Ticks:");
+		centerPanel.add(ticksLabel);
+		ticksSpinner = new JSpinner(new SpinnerNumberModel(0, 0, 1000, 1));
+		centerPanel.add(ticksSpinner);
+		
+		
+		//Cancel and Ok Buttons
+		JPanel bottomPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 2, 3));
+		mainPanel.add(bottomPanel);
+		
+		cancelButton = createButton("Cancel");
+		bottomPanel.add(cancelButton);
+		cancelButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				status = 0;
+				setVisible(false);
+			}
 
-    private void initGui() {
-        JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
-        add(mainPanel);
-
-        // Description
-        JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 2, 3));
-        mainPanel.add(topPanel);
-
-        JLabel descLabel = new JLabel("Schedule an event to change the CO2 of a vehicle after a given number o simulation ticks from now");
-        topPanel.add(descLabel);
-
-        // Vehicles CO2 class and Ticks
-        JPanel centerPanel = new JPanel();
-        mainPanel.add(centerPanel);
-
-        JLabel ticksLabel = new JLabel("Ticks: ");
-        centerPanel.add(ticksLabel);
-        ticksSpinner = new JSpinner(new SpinnerNumberModel(0, 0, 1000, 1));
-        centerPanel.add(ticksSpinner);
-
-        // Cancel  and Ok Buttons
-        JPanel bottomPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 2, 3));
-        mainPanel.add(bottomPanel);
-
-        cancelButton = new JButton("Cancel");
-        bottomPanel.add(cancelButton);
-        cancelButton.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                setVisible(false);
-            }
-        });
-
-        okButton = new JButton("Ok");
-        bottomPanel.add(okButton);
-        okButton.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-
-        pack();
-        setName("Change CO2 class");
-
-    }
+		});
+		
+		okButton = createButton("Ok");
+		bottomPanel.add(okButton);
+		okButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				status = 1;
+				setVisible(false);
+			}
+		});
+		
+		pack();
+		setName("Change CO2 Class");
+		setVisible(true);
+	}
+	
+	int getStatus() { return status; }
+	
+	int open(List<Vehicle> vehicles) {
+		
+		boxVehicles.removeAllItems();
+		for (Vehicle v : vehicles)
+			boxVehicles.addItem(v.toString());
+		
+		boxCO2.removeAllItems();
+		for (int i = 0; i < 10; i++)
+			boxCO2.addItem(i);
+		
+		return status;
+	}
+	
+	private JButton createButton(String name) {
+		JButton returnButton = new JButton(name);
+		return returnButton;
+	}
+	
+	List<Pair<String, Integer>> getNewCO2Vehicle() {
+		List<Pair<String, Integer>> data = new ArrayList<>();
+		String v = (String) boxVehicles.getSelectedItem().toString();
+		Integer co2 = (Integer) boxCO2.getSelectedItem();
+		data.add(new Pair<String, Integer>(v, co2));
+		return data;
+	}
+	
+	int getTime() { return (int) ticksSpinner.getValue(); }
 }
