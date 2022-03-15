31c62cc5a8321cbf64946d691318f6eea4d7c5c1
Jaime Jiménez <jaimejimenez99@hotmail.com>
2022-03-15 21:54:07 +0100

    RoadsTableModel class

--- simulator/view/RoadsTableModel.java
+++ simulator/view/RoadsTableModel.java
@@ -1,10 +1,13 @@
 package simulator.view;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.table.AbstractTableModel;
 
+import simulator.control.Controller;
 import simulator.model.Event;
+import simulator.model.Road;
 import simulator.model.RoadMap;
 import simulator.model.TrafficSimObserver;
 
@@ -12,21 +15,43 @@ public class RoadsTableModel extends AbstractTableModel implements TrafficSimObs
 
 	private static final long serialVersionUID = 1L;
 
-	@Override
-	public int getRowCount() {
-		// TODO Auto-generated method stub
-		return 0;
+	private static final String[] columnNames = { "Id", "Length", "Weather", "Max. Speed", "Speed Limit", "Total CO2", "CO2 Limit" };
+
+	private List<Road> roads;
+	
+	RoadsTableModel(Controller ctrl) {
+		roads = new ArrayList<>();
+		ctrl.addObserver(this);
 	}
 
 	@Override
-	public int getColumnCount() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+	public int getRowCount() { return roads.size(); }
+
+	@Override
+	public String getColumnName(int i) { return columnNames[i]; }
+	
+	@Override
+	public int getColumnCount() { return columnNames.length; }
 
 	@Override
 	public Object getValueAt(int rowIndex, int columnIndex) {
-		// TODO Auto-generated method stub
+		switch(columnIndex) {
+		case 0:
+			return roads.get(rowIndex).getId();
+		case 1:
+			return roads.get(rowIndex).getLength();
+		case 2:
+			return roads.get(rowIndex).getWeather();
+		case 3:
+			return roads.get(rowIndex).getMaxSpeed();
+		case 4:
+			return roads.get(rowIndex).getSpeedLimit();
+		case 5:
+			return roads.get(rowIndex).getTotalCO2();
+		case 6:
+			return roads.get(rowIndex).getCO2Limit();
+		}
+		
 		return null;
 	}
 
@@ -38,8 +63,8 @@ public class RoadsTableModel extends AbstractTableModel implements TrafficSimObs
 
 	@Override
 	public void onAdvanceEnd(RoadMap map, List<Event> events, int time) {
-		// TODO Auto-generated method stub
-		
+		this.roads = map.getRoads();
+		fireTableDataChanged();
 	}
 
 	@Override
@@ -50,8 +75,8 @@ public class RoadsTableModel extends AbstractTableModel implements TrafficSimObs
 
 	@Override
 	public void onReset(RoadMap map, List<Event> events, int time) {
-		// TODO Auto-generated method stub
-		
+		this.roads = new ArrayList<>();
+		fireTableDataChanged();
 	}
 
 	@Override
